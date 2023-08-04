6ffbb6617f59cf98b31bda3e74ae7392c19e638e
asfamumtaz <asfamumtaz@gmail.com>
2023-08-04 16:26:28 +0500

    update code

--- scripts/python/fetch-params/gitlab.py
+++ scripts/python/fetch-params/gitlab.py
@@ -2,10 +2,10 @@ import json
 import requests
 from requests.auth import HTTPBasicAuth
 
-def send_api_request_github(url, password):
+def send_api_request_gitlab(url, password):
 
    try:
-       headers = {"Authorization": f"token {password}"}
+       headers = {"PRIVATE-TOKEN": password}
        response = requests.get(url, headers=headers)
        if response.status_code == 200:
            parsed_data = json.loads(response.text)
@@ -18,21 +18,20 @@ def send_api_request_github(url, password):
      print(f"Error occurred during the API request: {e}")
      return None
 
-def fetch_params_github(provider, username, password, hash, workspace, repository):
+def fetch_params_gitlab(provider, password, hash, project_id):
 
-    if provider == "github":
-      url = f"https://api.github.com/repos/{workspace}/{repository}/pulls"
-      print(url)
-      pull_requests = send_api_request (url, password)
+    if provider == "gitlab":
+      url = f"{base_url}/api/v4/projects/{project_id}/merge_requests"
+      pull_requests = send_api_request(url, password)
       if pull_requests:
         for pr in pull_requests:
-          pull_request_id = pr['number']
-          url = f"https://api.github.com/repos/{workspace}/{repository}/pulls/{pull_request_id}/commits"
-          commits = send_api_request(url,"", password, "github")
+          pull_request_id = pr['id']
+          url = f"{base_url}/api/v4/projects/{project_id}/merge_requests/{pull_request_id}/commits"
+          commits = send_api_request(url, password)
           if commits:
             for commit in commits:
-              print(f"Commit SHA: {commit['sha']}")
-              if commit['sha'] == hash:
+              print(f"Commit SHA: {commit['id']}")
+              if commit['id'] == hash:
                 print(f"Found hash in PR {pull_request_id}")
                 found = True
                 break
