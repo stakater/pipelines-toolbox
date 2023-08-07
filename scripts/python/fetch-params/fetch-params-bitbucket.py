35460fd6352ae1bee12b17c42acf4200e4eaefb8
asfamumtaz <asfamumtaz@gmail.com>
2023-08-07 16:26:12 +0500

    update script

--- scripts/python/fetch-params/fetch-params-bitbucket.py
+++ scripts/python/fetch-params/fetch-params-bitbucket.py
@@ -46,26 +46,14 @@ def fetch_params_bitbucket(provider, username, password, hash, workspace, reposi
               print(f"Found hash in PR {pull_request_id}")
               found = True
               break
-# def main(args):
-#     provider = args.provider
-#     username = args.username
-#     password = args.password
-#     hash = args.hash
-#     workspace = args.workspace
-#     repository = args.repository
-#     fetch_params_bitbucket(provider, username, password, hash, workspace, repository)
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("script", help="The number for which to calculate square and cube.")
-    parser.add_argument("provider", help="The number for which to calculate square and cube.")
-    parser.add_argument("username", help="The number for which to calculate square and cube.")
-    parser.add_argument("password", help="The number for which to calculate square and cube.")
-    parser.add_argument("hash", help="The number for which to calculate square and cube.")
-    parser.add_argument("workspace", help="The number for which to calculate square and cube.")
-    parser.add_argument("repository",  help="The number for which to calculate square and cube.")
+    parser.add_argument("provider", help="Name of git provider")
+    parser.add_argument("username", help="Username for git provider")
+    parser.add_argument("password", help="Password/Token for provider")
+    parser.add_argument("hash", help="Hash of the commit that triggered the pipeline")
+    parser.add_argument("workspace", help="Workspace/Organization")
+    parser.add_argument("repository",  help="Git repository name")
     args = parser.parse_args()
     fetch_params_bitbucket(args.provider, args.username, args.password, args.hash, args.workspace, args.repository)
-#     main(args)
