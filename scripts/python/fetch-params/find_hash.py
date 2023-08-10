3bca8996bf5ccf89babc3e28d3b12200001cfd5d
asfamumtaz <asfamumtaz@gmail.com>
2023-08-11 02:29:32 +0500

    failure case added

--- scripts/python/fetch-params/find_hash.py
+++ scripts/python/fetch-params/find_hash.py
@@ -3,11 +3,12 @@ from fetch_params_github import fetch_params_github
 import argparse
 
 def find_hash(provider, username, password, hash, workspace, repository):
+  pr_number = None
   if provider == "bitbucket":
     pr_number = fetch_params_bitbucket(provider, username, password, hash, workspace, repository)
   elif provider == "github":
     pr_number = fetch_params_github(provider, username, password, hash, workspace, repository)
-
+  return pr_number
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("provider", help="Name of git provider")
